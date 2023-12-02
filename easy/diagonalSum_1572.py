b3efe12951ff633f61e5a8d55bf6272e76058ba4
yuchuanqi <406170>
2023-12-02 14:54:56 +0800

    【模块名称】：无 【问题原因】: 无 【修改描述】:无 【影响点】: test 【问题/需求单号】 test

--- easy/diagonalSum_1572.py
+++ easy/diagonalSum_1572.py
@@ -1,4 +1,5 @@
 class Solution:
+    # 方法一，求出主对角线和副对角线和，在减去重叠部分的值，n阶矩阵，当n是奇数时，存在重叠，是偶数时，不存在重叠，使用(n-1)&1 判断是否是奇数，n-1是因为数组下标从0开始
     def diagonalSum_1(self, mat) -> int:
         sum=0
         l=len(mat)
