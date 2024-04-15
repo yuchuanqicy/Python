66f94f2f0571734c6effbfba5902286678fc5eaa
yuchuanqi <406170>
2024-04-15 18:55:05 +0800

    每日一题  205

---
+++ easy/MyQueue_232.py
@@ -0,0 +1,28 @@
+'''
+@Project ：Python 
+@File    ：MyQueue_232.py
+@Author  ：Master
+@Date    ：2024/4/15 18:25 
+'''
+class Stack:
+    def __init__(self):
+        self.stack = []
+
+    def is_empty(self):
+        return len(self.stack) == 0
+
+    def push(self, item):
+        self.stack.append(item)
+
+    def pop(self):
+        if self.is_empty():
+            return None
+        return self.stack.pop()
+
+    def peek(self):
+        if self.is_empty():
+            return None
+        return self.stack[-1]
+
+    def size(self):
+        return len(self.stack)
--- easy/majorityElement_169.py
+++ easy/majorityElement_169.py
@@ -17,3 +17,39 @@ class Solution:
         '''
         res=Counter(nums)
         return max(res.keys(), key=res.get)
+
+
+    def majorityElement_2(self, nums: List[int]) -> int:
+        '''
+        方法二、使用自己构造Counter函数来统计为数组元素出现的次数,大于数组长度len/2，返回计数最大的数
+        :param nums:
+        :return:
+        '''
+        countdic={}
+        for num in nums:
+            if num in countdic:
+                countdic[num]+=1
+            else:
+                countdic[num]=1
+        ans = max(countdic.values())
+        if ans > len(nums) // 2:
+            for k,v in countdic.items():
+                if v==ans:
+                    return k
+        else:
+            return None
+    def majorityElement_3(self, nums: List[int]) -> int:
+        '''
+        方法三、使用Boyer-Moore 算法来找出众数
+        :param nums:
+        :return:
+        '''
+        count=0
+        candate=0
+        for num in nums:
+            if count==0:
+                candate=num
+            count+=(1 if num==candate else -1)
+        return candate
+
+
