3053abd561a4299f4276463d4d9124cc92199e16
yuchuanqi <406170>
2024-04-18 18:26:16 +0800

    每日一题  278

---
+++ easy/firstBadVersion_278.py
@@ -0,0 +1,11 @@
+'''
+@Project ：Python 
+@File    ：firstBadVersion_278.py
+@Author  ：Master
+@Date    ：2024/4/18 10:45 
+'''
+
+
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        pass
