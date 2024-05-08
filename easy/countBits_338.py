8c46b7080ae10402fab1fbaf3da29f9bad497ac1
YCQ <1334729724@qq.com>
2024-05-08 22:40:24 +0800

    每日一题  338

--- easy/countBits_338.py
+++ easy/countBits_338.py
@@ -72,4 +72,7 @@ class Solution:
         :param n:
         :return:
         '''
-        pass
+        count=[0]*(n+1)
+        for i  in range(1,n+1):
+            count[i]=count[i>>1]+(i&1)
+        return count
