51f7ede21153f692aa5344d00b0e34d0acc6e30a
yuchuanqi <406170>
2024-05-14 22:08:34 +0800

    每日一题  349

--- easy/intersection_349.py
+++ easy/intersection_349.py
@@ -8,7 +8,13 @@ from typing import List
 
 
 class Solution:
-    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+    def intersection1(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        '''
+        暴力方法优化，时间复杂度优化为2n
+        :param nums1:
+        :param nums2:
+        :return:
+        '''
         len1, len2 = len(nums1), len(nums2)
         res = []
         if len1 > len2:
@@ -22,5 +28,11 @@ class Solution:
                 if nums1[i] in nums2 and nums1[i] not in res:
                     res.append(nums1[i])
             return res
-
+    def intersection2(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        '''
+        集合操作
+        :param nums1:
+        :return:
+        '''
+        return list(set(nums1) & set(nums2))
 
