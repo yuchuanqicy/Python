beea63d7ce6334bef818a1136c9b2b33bbc42398
yuchuanqi <406170>
2024-05-17 14:43:42 +0800

    每日一题  350

--- easy/intersect_350.py
+++ easy/intersect_350.py
@@ -4,10 +4,11 @@
 @Author  ：Master
 @Date    ：2024/5/15 11:06 
 '''
+import collections
 from typing import List
 
 class Solution:
-    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+    def intersect1(self, nums1: List[int], nums2: List[int]) -> List[int]:
         '''
         hash表做法,需要额外的存储空间
         :param nums1:
@@ -26,4 +27,14 @@ class Solution:
                 res.append(i)
                 ans[i]-=1
         return res
-
+    def intersect2(self, nums1: List[int],nums2: List[int]) -> List[int]:
+        '''
+        高级做法，Counter  返回结果 &交集
+        :param nums1:
+        :param nums2:
+        :return:
+        '''
+        n1=collections.Counter(nums1)
+        n2=collections.Counter(nums2)
+        res=n1 & n2
+        return list(res.elements())
