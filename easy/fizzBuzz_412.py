22a186e5385f33f3d5032c25883c9648c438fc00
yuchuanqi <406170>
2024-08-26 21:13:28 +0800

    每日一题  412

---
+++ easy/fizzBuzz_412.py
@@ -0,0 +1,16 @@
+'''
+@Project ：Python 
+@File    ：fizzBuzz_412.py
+@Author  ：Master
+@Date    ：2024/8/26 21:11 
+'''
+from typing import List
+
+
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        for i in range(n):
+            StrRes=[]
+            if i % 3 == 0 and i % 5 == 0:
+                pass
+        pass
