f3a1bb0e8fef65c8542b49b88a61d9e5311b4901
yuchuanqi <406170>
2024-08-29 09:27:49 +0800

    每日一题  412

--- easy/fizzBuzz_412.py
+++ easy/fizzBuzz_412.py
@@ -8,9 +8,24 @@ from typing import List
 
 
 class Solution:
-    def fizzBuzz(self, n: int) -> List[str]:
-        for i in range(n):
-            StrRes=[]
+    def fizzBuzz_1(self, n: int) -> List[str]:
+        StrRes = []
+        for i in range(1,n+1):
             if i % 3 == 0 and i % 5 == 0:
-                pass
-        pass
+                StrRes.append('FizzBuzz')
+            elif i % 3 == 0 :
+                StrRes.append('Fizz')
+            elif i%5==0:
+                StrRes.append('Buzz')
+            else:
+                StrRes.append(f"{i}")
+        return StrRes
+    def fizzBuzz_1(self, n: int) -> List[str]:
+        ans=[]
+        for i in range(1,n+1):
+            flag1,flag2= i%3==0,i%5==0
+            if flag1 or flag2:
+                ans.append('Fizz'*flag1+'Buzz'*flag2)
+            else:
+                ans.append(str(i))
+        return ans
